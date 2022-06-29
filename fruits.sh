b1cda703bb4caa16504af475ca94f5c15d769192
Ubuntu <ubuntu@ip-172-31-35-236.ap-south-1.compute.internal>
2022-06-29 06:04:11 +0000

    this is new file

---
+++ fruits.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "This is hari"
+echo "enter the cost of apple"
+read a
+echo "enter the cost of mango"
+read b
+echo "enter tha cost of banana"
+read c
+echo -e "the cost of apple is $a \n cost of mango is $b \n cost of banana is $c"
+
